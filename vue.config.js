7a59691cff99da609bf4d7f74fbd75416ff2b192
mauroaicode <mauricioaicode2310@gmail.coml>
2023-01-14 00:03:28 -0500

    ajuste de cors V4

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  devServer: {
-    proxy: process.env.VUE_APP_BACK_URL,
-  },
   transpileDependencies: true
 })
